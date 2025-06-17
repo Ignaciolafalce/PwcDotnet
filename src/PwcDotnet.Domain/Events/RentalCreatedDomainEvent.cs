a403e287dbf21d8aae4befeff033f0b036b23583
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-17 16:48:52 -0300

    Rental created domain event added (later used for email send)

---
+++ src/PwcDotnet.Domain/Events/RentalCreatedDomainEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace PwcDotnet.Domain.Events
+{
+
+    public class RentalCreatedDomainEvent : DomainEvent
+    {
+        public Rental Rental { get; }
+        public int RentalId { get; }
+        public int CustomerId { get; }
+        public int CarId { get; }
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+
+        public RentalCreatedDomainEvent(Rental rental)
+        {
+            Rental = rental;
+            RentalId = rental.Id;
+            CustomerId = rental.CustomerId;
+            CarId = rental.CarId;
+            StartDate = rental.Period.Start;
+            EndDate = rental.Period.End;
+        }
+    }
+}
