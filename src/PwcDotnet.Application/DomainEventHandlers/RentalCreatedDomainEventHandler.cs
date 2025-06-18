eb525521115762534bb79d769f68d29d7a21c52c
Ignacio lafalce <nacho_lafalce@hotmail.com>
2025-06-17 22:13:12 -0300

    RentalCreatedDomainEventHandler added (as placeholder we must add implementation)

---
+++ src/PwcDotnet.Application/DomainEventHandlers/RentalCreatedDomainEventHandler.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace PwcDotnet.Application.DomainEventHandlers;
+
+public class RentalCreatedDomainEventHandler : INotificationHandler<RentalCreatedDomainEvent>
+{
+
+    // repositories, services, or other dependencies can be injected here
+    private readonly ILogger _logger;
+
+    public RentalCreatedDomainEventHandler(ILogger logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public Task Handle(RentalCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        // Invoke durable function with azurite send email!
+        return Task.CompletedTask;
+    }
+}
